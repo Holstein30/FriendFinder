c6092903e62b5bc7023582c7fead2ed4b3a294b3
Dale Holstein <DaleHolstein@Dales-MacBook-Pro.local>
2017-11-22 21:18:44 -0500

    finished

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,29 +1,125 @@
 var friendsData = [{
-        name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        name: "Gandalf",
+        photo: "https://i.imgur.com/7qjvKyi.jpg",
         scores: [
-            "5",
-            "1",
-            "4",
-            "4",
-            "5",
             "1",
             "2",
             "5",
+            "5",
+            "1",
+            "1",
+            "1",
             "4",
+            "1",
+            "5"
+        ]
+    },
+    {
+        name: "Aragorn",
+        photo: "https://vignette.wikia.nocookie.net/lotr/images/c/ce/King_Aragorn.PNG/revision/latest?cb=20121013121849",
+        scores: [
+            "1",
+            "1",
+            "5",
+            "2",
+            "2",
+            "1",
+            "1",
+            "3",
+            "1",
+            "3"
+        ]
+    },
+    {
+        name: "Gimli",
+        photo: "https://sphoto.nasza-klasa.pl/50116577/9/main/122a834f7f.jpeg",
+        scores: [
+            "1",
+            "3",
+            "5",
+            "3",
+            "3",
+            "1",
+            "5",
+            "1",
+            "2",
             "1"
         ]
     },
     {
-        name: "Jacob Deming",
-        photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        name: "Legolas",
+        photo: "https://screenrant.com/wp-content/uploads/2017/02/Orlando-Bloom-as-Legolas-Greenleaf-The-Lord-of-the-Rings-Fellowship-of-the-Ring-Bow.jpg",
         scores: [
-            "4",
+            "1",
             "2",
             "5",
             "1",
+            "1",
+            "1",
+            "1",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Boromir",
+        photo: "http://vignette4.wikia.nocookie.net/factpile/images/6/66/Lotr-boromir-1280jpg-b6a4d5_1280w.jpg/revision/latest?cb=20160331232305",
+        scores: [
+            "1",
+            "3",
+            "1",
+            "2",
+            "2",
+            "4",
+            "2",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: "Eowyn",
+        photo: "https://vignette.wikia.nocookie.net/lotr/images/e/e0/Miranda_Otto_as_Eowyn.png/revision/latest/scale-to-width-down/175?cb=20121023104837",
+        scores: [
+            "1",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "4",
+            "5",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Arwen",
+        photo: "https://vignette.wikia.nocookie.net/seigneur-des-anneaux/images/f/f9/Arwen2.jpg/revision/latest?cb=20140329102507&path-prefix=fr",
+        scores: [
             "3",
             "2",
+            "4",
+            "1",
+            "1",
+            "1",
+            "4",
+            "2",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Frodo",
+        photo: "https://i.pinimg.com/736x/65/7d/c2/657dc2ef6e8022e571f72ae09bb035f5--hobbit-hole-the-hobbit.jpg",
+        scores: [
+            "1",
+            "4",
+            "4",
+            "2",
+            "1",
+            "4",
             "2",
             "1",
             "3",
@@ -31,67 +127,115 @@ var friendsData = [{
         ]
     },
     {
-        name: "Jeremiah Scanlon",
-        photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        name: "Sam",
+        photo: "https://i.pinimg.com/736x/f3/8f/f0/f38ff01fde4f248cba48524e929f59a1--samwise-gamgee-lord-of-the-rings.jpg",
         scores: [
+            "1",
+            "4",
             "5",
             "2",
-            "2",
-            "2",
-            "4",
             "1",
             "3",
+            "1",
             "2",
-            "5",
-            "5"
+            "2",
+            "2"
         ]
     },
     {
-        name: "Louis T. Delia",
-        photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        name: "Eomer",
+        photo: "https://vignette.wikia.nocookie.net/lotr/images/b/b9/Eomer_-_Close_up.PNG/revision/latest/scale-to-width-down/350?cb=20120922113500",
         scores: [
+            "1",
             "3",
             "3",
-            "4",
+            "2",
             "2",
             "2",
             "1",
-            "3",
-            "2",
-            "2",
+            "5",
+            "1",
             "3"
         ]
     },
     {
-        name: "Lou Ritter",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        name: "Pippin",
+        photo: "https://s-media-cache-ak0.pinimg.com/originals/76/e8/ba/76e8bae41a263d83fd49dde78a7a6226.jpg",
         scores: [
-            "4",
-            "3",
-            "4",
-            "1",
-            "5",
             "2",
             "5",
+            "4",
+            "2",
+            "4",
+            "1",
+            "2",
+            "4",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: "Galadriel",
+        photo: "https://upload.wikimedia.org/wikipedia/en/c/cb/Galadriel.jpg",
+        scores: [
+            "4",
+            "1",
             "3",
+            "5",
+            "1",
+            "2",
+            "1",
+            "1",
             "1",
             "4"
         ]
     },
     {
-        name: "Jordan Biason",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        name: "Saruman",
+        photo: "https://vignette.wikia.nocookie.net/factpile/images/9/94/3-hobbit_saruman.jpg/revision/latest?cb=20130419023826",
         scores: [
+            "3",
+            "1",
+            "1",
+            "5",
             "4",
             "4",
+            "1",
             "2",
             "3",
-            "2",
-            "2",
-            "3",
-            "2",
-            "4",
             "5"
+        ]
+    },
+    {
+        name: "Sauron",
+        photo: "https://vignette.wikia.nocookie.net/lotr/images/3/3a/Sauron.jpg/revision/latest?cb=20120620000759",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "4",
+            "5",
+            "4",
+            "1",
+            "1",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Gollum",
+        photo: "https://nerdist.com/wp-content/uploads/2017/03/gollum-smeagol-singing.jpg",
+        scores: [
+            "1",
+            "3",
+            "1",
+            "1",
+            "3",
+            "5",
+            "2",
+            "1",
+            "2",
+            "2"
         ]
     }
 ];
